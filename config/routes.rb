705726d6814ac8166e62b10c324490d65f600de6
abedaiki <abe12261994@gmail.com>
2025-04-16 12:21:46 +0000

    Add ユーザーのコントローラ、ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "homes#top"
   get"/homes/about" => "homes#about" ,as: "about"
+  devise_for :users
+  resources :users, only: [:edit, :update,:index, :show]
 end

71bbd44f53fc29dc96e7ece50f6a593d51e0463c
abedaiki <abe12261994@gmail.com>
2025-04-18 10:57:12 +0000

    Add 本の新規投稿機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,6 @@
 class BooksController < ApplicationController
-  def new
-  end
-
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def index
   end
 
@@ -11,12 +10,44 @@ class BooksController < ApplicationController
   def edit
   end
 
-  def create
-  end
-
   def destroy
   end
 
   def update
   end
+
+  # 新規投稿画面
+  def new
+    @book = Book.new
+  end
+
+  # 投稿処理
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+      if @book.save
+        flash[:notice] = "Book was successfully created."
+        redirect_to book_path(@book.id)
+      else
+        @user = User.find(current_user.id)
+        @books = Book.page(params[:page])
+          flash.now[:alert] = "Book was make a mistake created."
+          render :index
+      end
+  end
+  
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
 end

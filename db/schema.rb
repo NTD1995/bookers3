bdde071dd98be9a4109c9b7e33b5358e25da7ea4
abedaiki <abe12261994@gmail.com>
2025-04-22 13:01:02 +0000

    Add コメントのモデル、テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_04_18_071459) do
+ActiveRecord::Schema.define(version: 2025_04_22_125605) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 2025_04_18_071459) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment", null: false
+    t.integer "user_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_book_comments_on_book_id"
+    t.index ["user_id"], name: "index_book_comments_on_user_id"
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
     t.string "body", null: false
@@ -65,5 +75,7 @@ ActiveRecord::Schema.define(version: 2025_04_18_071459) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "book_comments", "books"
+  add_foreign_key "book_comments", "users"
   add_foreign_key "books", "users"
 end

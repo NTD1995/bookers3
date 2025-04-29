075578dbc7f4e9f5afacd674ead7764270018f68
abedaiki <abe12261994@gmail.com>
2025-04-29 09:21:49 +0000

    Add いいね機能のコントローラー、ビュー、ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update,:index, :show]
   resources :books do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end  
 end

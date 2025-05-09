0fcb31aad6408a70946424fb456f916a319c554b
abedaiki <abe12261994@gmail.com>
2025-05-09 13:35:19 +0000

    Fix DM機能の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,27 +26,37 @@ class UsersController < ApplicationController
   end
 
   # ユーザー詳細画面
-  def show
-    @user = User.find(params[:id])
-    @book = Book.new
-    @currentUserEntry = Entry.where(user_id: current_user.id)
-    @userEntry = Entry.where(user_id: @user.id)
-    unless @user.id == current_user.id
-      @currentUserEntry.each do |cu| 
-        @userEntry.each do |u| 
-          if cu.room_id == u.room_id 
+ def show
+  @user = User.find(params[:id])
+  @book = Book.new
+  @currentUserEntry = Entry.where(user_id: current_user.id)
+  @userEntry = Entry.where(user_id: @user.id)
+  
+  unless @user.id == current_user.id
+    @isRoom = false
+    if @currentUserEntry.any? && @userEntry.any?
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id
             @isRoom = true
             @roomId = cu.room_id
+            break
           end
         end
+        break if @isRoom
       end
-      if @isRoom
-      else
-        @room = Room.new
-        @entry = Entry.new
-      end
-    end  
+    end
+
+
+        Rails.logger.debug("isRoom: #{@isRoom}")
+    Rails.logger.debug("roomId: #{@roomId}")
+    
+    unless @isRoom
+      @room = Room.new
+      @entry = Entry.new
+    end
   end
+end
 
   private
 

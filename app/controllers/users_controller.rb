7c931659b3c3a531fc9e885f5aa9eaf64ce88a64
abedaiki <abe12261994@gmail.com>
2025-05-11 14:14:04 +0000

    Fix DM機能の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,40 +23,30 @@ class UsersController < ApplicationController
     @users = User.page(params[:page])
     @user = current_user
     @book = Book.new
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |cu| 
+        @userEntry.each do |u| 
+          if cu.room_id == u.room_id 
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end 
   end
 
   # ユーザー詳細画面
- def show
-  @user = User.find(params[:id])
-  @book = Book.new
-  @currentUserEntry = Entry.where(user_id: current_user.id)
-  @userEntry = Entry.where(user_id: @user.id)
-  
-  unless @user.id == current_user.id
-    @isRoom = false
-    if @currentUserEntry.any? && @userEntry.any?
-      @currentUserEntry.each do |cu|
-        @userEntry.each do |u|
-          if cu.room_id == u.room_id
-            @isRoom = true
-            @roomId = cu.room_id
-            break
-          end
-        end
-        break if @isRoom
-      end
-    end
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
 
-
-        Rails.logger.debug("isRoom: #{@isRoom}")
-    Rails.logger.debug("roomId: #{@roomId}")
-    
-    unless @isRoom
-      @room = Room.new
-      @entry = Entry.new
-    end
   end
-end
 
   private
 
